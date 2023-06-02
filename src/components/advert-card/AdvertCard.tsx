3b2d5d217adf263badcf8e9fb52c2bc9fddb6ba8
Lexeor <lexeor.ross@gmail.com>
2023-06-02 09:16:01 +0400

    AdvertCard TSX migration

---
+++ src/components/advert-card/AdvertCard.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import "./advert-card.css";
+
+type AdvertCardProps = {
+  price: number;
+  title: string;
+  details: string;
+  newChats: number;
+  totalChats: number;
+  lastUpdated: Date;
+};
+
+function AdvertCard({
+  price,
+  title,
+  details,
+  newChats,
+  totalChats,
+  lastUpdated,
+}: AdvertCardProps) {
+  const newChatsStyle = newChats > 0 ? "active" : "";
+  return (
+    <div className="adv-card">
+      <img
+        src="https://picsum.photos/300/200"
+        alt="Preview"
+        className="adv-card-preview"
+      />
+      <div className="adv-card-content">
+        <div className="add-card-updated">
+          <p>Last updated</p>
+          <p>{lastUpdated.toString()}</p>
+        </div>
+        <div className="adv-card-price">{price}</div>
+        <div className="adv-card-title">{title}</div>
+        <div className="adv-card-details">{details}</div>
+      </div>
+      <button>
+        <i className="ri-chat-1-line"></i>
+        <p>Chats</p>
+        <span className={newChatsStyle}>
+          {newChats > 0 ? newChats : totalChats}
+        </span>
+      </button>
+    </div>
+  );
+}
+
+export default AdvertCard;
