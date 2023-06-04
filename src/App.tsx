34738840a25431964e7077b595c91bc769f086b5
Lexeor <lexeor.ross@gmail.com>
2023-06-04 12:01:11 +0400

    App component migration to TSX

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Popup from "./components/popup/Popup";
 import MenuPopup from "./components/menu-popup/MenuPopup";
